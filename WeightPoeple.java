da05b2f5fb82fc85a29d3f27857d60a7b834c6eb
Saba ICT Co <mahdiyarmerrikhi2008@gmail.com>
2023-08-01 19:57:54 +0330

    Java Code About Weight on Plans.

---
+++ WeightPoeple.java
@@ -0,0 +1,16 @@
+public class WeightPoeple {
+    public static void main(String[] args) {
+        double myWeightOnEarth = 85.5;
+        // Earth
+        System.out.print("My weight on the Earth ");
+        System.out.println(myWeightOnEarth);
+        // Jupiter
+        System.out.print("My weight on the Jupiter ");
+        double myWeightOnJupiter = myWeightOnEarth * 2.52;
+        System.out.println(myWeightOnJupiter);
+        // Saturn
+        System.out.print("My weight on the Saturn ");
+        double myWeightOnSaturn = myWeightOnEarth * 1.06;
+        System.out.println(myWeightOnSaturn);
+    }
+}
